054bc798c77e320d3610368db7ceb981a1182597
Jhony Veizaga S. <39717129+JhonyVS@users.noreply.github.com>
2026-03-10 22:02:36 -0400

    segundo commit

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com  as
-  apiUrl: 'https://maneja-tus-gastos-api-h079.onrender.com/',
+  apiUrl: 'https://maneja-tus-gastos-api-h079.onrender.com',
 };
 
